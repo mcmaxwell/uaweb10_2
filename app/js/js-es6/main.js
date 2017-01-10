d889e584b23a830e1d80ce39a258771fcd1c0fb5
Maxim Liutsko <maximliutsko@Maxims-MacBook-Pro.local>
2017-01-10 13:51:15 +0200

    add

---
+++ app/js/js-es6/main.js
@@ -0,0 +1,144 @@
+
+const date = new Date(),
+      menu = document.querySelector('.main-menu');
+
+//add year in footer
+document.querySelector('#year').innerHTML = date.getFullYear();
+
+//toogleMenu
+const toogleMenu = (e) => {
+  document.querySelector('.main-menu-list').classList.toggle('active');
+  document.querySelector('.toggle-main-menu').classList.toggle('active');
+  document.querySelector('body').classList.toggle('fixed');
+  e.preventDefault();
+};
+
+document.querySelector('.main-menu').addEventListener('click', (e) => {
+  if (hasClass(e.target, 'toggle-main-menu')){
+    toogleMenu(e);
+  } else if (e.target.className === 'main-menu-link') {
+    scrollToY(document.querySelector('.' + e.target.getAttribute('href')).offsetTop, 1500, 'easeInOutQuint');
+    toogleMenu(e);
+    e.preventDefault();
+  } else  return false
+});
+
+//requestAnimFrame function
+window.requestAnimFrame = (function(){
+  return  window.requestAnimationFrame       ||
+          window.webkitRequestAnimationFrame ||
+          window.mozRequestAnimationFrame    ||
+          function( callback ){
+            window.setTimeout(callback, 1000 / 60);
+          };
+})();
+
+//has ClassName
+function hasClass( elem, className ) {
+     return (" " + elem.className + " " ).indexOf( " "+className+" " ) > -1;
+}
+
+// scroll to el function
+function scrollToY(scrollTargetY, speed, easing) {
+
+    var scrollY = window.scrollY,
+        scrollTargetY = scrollTargetY || 0,
+        speed = speed || 2000,
+        easing = easing || 'easeOutSine',
+        currentTime = 0;
+
+    let time = Math.max(.1, Math.min(Math.abs(scrollY - scrollTargetY) / speed, .8));
+
+    let PI_D2 = Math.PI / 2,
+        easingEquations = {
+            easeOutSine: function (pos) {
+                return Math.sin(pos * (Math.PI / 2));
+            },
+            easeInOutSine: function (pos) {
+                return (-0.5 * (Math.cos(Math.PI * pos) - 1));
+            },
+            easeInOutQuint: function (pos) {
+                if ((pos /= 0.5) < 1) {
+                    return 0.5 * Math.pow(pos, 5);
+                }
+                return 0.5 * (Math.pow((pos - 2), 5) + 2);
+            }
+        };
+
+    function tick() {
+        currentTime += 1 / 60;
+
+        let p = currentTime / time;
+        let t = easingEquations[easing](p);
+
+        if (p < 1) {
+            requestAnimFrame(tick);
+
+            window.scrollTo(0, scrollY + ((scrollTargetY - scrollY) * t));
+        } else {
+            window.scrollTo(0, scrollTargetY);
+        }
+    }
+
+    tick();
+}
+
+// throttle function
+  function throttle(func, ms) {
+
+    let isThrottled = false,
+      savedArgs,
+      savedThis;
+
+    function wrapper() {
+
+      if (isThrottled) {
+        savedArgs = arguments;
+        savedThis = this;
+        return;
+      }
+
+      func.apply(this, arguments);
+
+      isThrottled = true;
+
+      setTimeout(function() {
+        isThrottled = false;
+        if (savedArgs) {
+          wrapper.apply(savedThis, savedArgs);
+          savedArgs = savedThis = null;
+        }
+      }, ms);
+    }
+
+    return wrapper;
+  }
+
+// parallax el
+const parallaxEl = document.querySelectorAll('.parallax');
+const parallax = () => {
+  const scrolltop = window.pageYOffset;
+  for (let i = 0; i < parallaxEl.length; i++) {
+    let data = parallaxEl[i].dataset;
+
+    data.direction? data.direction: data.direction = 0
+    data.position? data.position: data.position = 0
+    data.speed? data.speed: data.speed = 0.2
+
+    let topPosition = (Number(data.direction + scrolltop) + Number(data.position * 2))* data.speed;
+    let direction = data.direction === '-'? topPosition < data.stop: topPosition > data.stop;
+
+    if (data.stop !== '' && direction) {
+      topPosition = data.stop;
+    } else {
+      topPosition
+    }
+
+    parallaxEl[i].style.transform = 'translateY(' + topPosition + 'px)';
+  }
+}
+  window.addEventListener('scroll', () => {
+    if (window.innerWidth > 1024) {
+      throttle( requestAnimationFrame(parallax), 50);
+    }
+  }, false)
---
+++ gulpfile.js
@@ -0,0 +1,196 @@
+const gulp = require('gulp'),
+      sass = require('gulp-sass'),
+      concatCSS = require('gulp-concat-css'),
+      livereload = require('gulp-livereload'),
+      connect = require('gulp-connect'),
+      useref = require('gulp-useref'),
+      autoprefixer = require('gulp-autoprefixer'),
+      gulpif = require('gulp-if'),
+      uglify = require('gulp-uglify'),
+      cssmin = require('gulp-clean-css'),
+      jshint = require("gulp-jshint"),
+      rigger = require('gulp-rigger'),
+      imagemin = require('gulp-imagemin'),
+      rimraf = require('rimraf'),
+      sourcemaps = require('gulp-sourcemaps'),
+      watch = require('gulp-watch'),
+      svgSprite = require('gulp-svg-sprite'),
+    	svgmin = require('gulp-svgmin'),
+    	cheerio = require('gulp-cheerio'),
+      babel = require('gulp-babel');
+
+const path = {
+    build: {
+        html: 'build/',
+        js: 'app/js/',
+        css: 'build/css/',
+        sass: 'app/css/',
+        img: 'build/img/',
+        fonts: 'build/fonts/',
+        sprites: 'app/img/',
+    },
+    app: {
+        html: 'app/*.html',
+        js: 'app/js/js-es6/*.js',
+        jshint: 'app/js/*.js',
+        sass: 'app/sass/*.scss',
+        img: 'app/img/**/*.*',
+        fonts: 'app/fonts/**/*.*',
+        sprites: 'app/sass/sprites/*.svg',
+    },
+    watch: {
+        html: 'app/*.html',
+        js: 'app/js/js-es6/*.js',
+        css: 'app/css/**/*.*',
+        sass: 'app/sass/*.scss',
+        img: 'app/img/**/*.*',
+        fonts: 'app/fonts/**/*.*',
+        sprites: 'app/sass/sprites/*.svg'
+    },
+    clean: './build',
+    rootDir: './build'
+};
+//server connect
+gulp.task('connect', () => {
+  connect.server({
+    root: [path.rootDir],
+    livereload: true
+  });
+});
+
+// sprites svg
+gulp.task('sprites:build', () => {
+    gulp.src(path.app.sprites)
+    .pipe(svgmin({
+      js2svg: {
+        pretty: true
+      }
+    }))
+    .pipe(cheerio({
+      run: function ($) {
+        $('[fill]').removeAttr('fill');
+				$('[stroke]').removeAttr('stroke');
+				$('[style]').removeAttr('style');
+      },
+      parserOptions: { xmlMode: true }
+    }))
+    .pipe(svgSprite({
+			mode: {
+				css : {
+					sprite: '../img/sprite.svg',
+          prefix : ".icon-%s",
+					render: {
+						scss: {
+							dest: '../sass/_sprite.scss'
+						}
+					}
+				}
+			}
+		}))
+    .pipe(gulp.dest('app/'));
+});
+
+//html
+gulp.task('html:build', () => {
+    gulp.src(path.app.html)
+    .pipe(rigger())
+    .pipe(useref())
+    .pipe(gulpif('*.js', uglify()))
+    .pipe(gulpif('*.css', cssmin()))
+    .pipe(gulp.dest(path.build.html))
+    .pipe(connect.reload());
+});
+
+//sass
+gulp.task('sass:build', () => {
+  gulp.src(path.app.sass)
+    .pipe(sass({
+      includePaths: [
+        'node_modules/foundation-sites/scss'
+      ]
+    }).on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions', 'ie >= 9'],
+        cascade: false
+    }))
+    .pipe(gulp.dest(path.build.sass))
+    .pipe(connect.reload());
+});
+
+//fonts
+gulp.task('fonts:build', () => {
+    gulp.src(path.app.fonts)
+    .pipe(gulp.dest(path.build.fonts));
+});
+
+//jshint
+gulp.task('jshint:build', () => {
+    gulp.src(path.app.jshint)
+    .pipe(jshint());
+});
+
+//js
+gulp.task('js:build', () => {
+    gulp.src(path.app.js)
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+            presets: ['es2015']
+        }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(path.build.js))
+    .pipe(connect.reload())
+});
+
+// image
+gulp.task('image:build', () => {
+    gulp.src(path.app.img)
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            interlaced: true,
+            optimizationLevel: 3
+        }))
+        .pipe(gulp.dest(path.build.img))
+        .pipe(connect.reload())
+});
+//other files
+gulp.task('other', () => {
+     gulp.src(['app/css/ajax-loader.gif', 'app/css/fonts/'])
+        .pipe(gulp.dest('build/css'));
+     gulp.src('app/css/fonts/*.*')
+        .pipe(gulp.dest('build/css/fonts'));
+});
+//clean
+gulp.task('clean', (cb) => {
+    rimraf(path.clean, cb);
+});
+
+// watch
+gulp.task('watch', () => {
+    watch([path.watch.html, 'app/include/*.html'], (event, cb) => {
+        gulp.start('html:build');
+    });
+    watch([path.watch.sprites], (event, cb) => {
+        gulp.start('sprites:build');
+    });
+    watch([path.watch.css], (event, cb) => {
+        gulp.start('html:build');
+    });
+    watch([path.watch.sass], (event, cb) => {
+        gulp.start('sass:build');
+    });
+    watch([path.watch.js], ['jshint']);
+    watch([path.watch.js], (event, cb) => {
+        gulp.start('js:build');
+    });
+    watch([path.watch.img], (event, cb) => {
+        gulp.start('image:build');
+    });
+    watch([path.watch.fonts], (event, cb) => {
+        gulp.start('fonts:build');
+    });
+});
+
+// default
+gulp.task('default', ['connect', 'other', 'sprites:build', 'html:build', 'sass:build', 'fonts:build', 'js:build', 'image:build', 'jshint:build', 'watch']);
